289b985f6b316ef9e7e0a3b9c5b52f3ba885afc6
Shpend Shabani <shpend@incodeks.com>
2021-06-14 11:08:39 +0200

    Changes in about us page

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,8 +1,8 @@
-$(document).on('scroll', function() {
-    if( $(this).scrollTop() >= $('#busineses').position().top - 100 ){
-        animateBussniessText();
-    }
-});
+// $(document).on('scroll', function() {
+//     if( $(this).scrollTop() >= $('#busineses').position().top - 100 ){
+//         animateBussniessText();
+//     }
+// });
 
 function animateBussniessText(){
     $('#busineses_intro_text').css('animation','animateBusniessText 1s');
